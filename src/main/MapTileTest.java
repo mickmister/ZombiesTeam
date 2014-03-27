cd72bb964e3f54ac478bbfbedf6f279912f1b235
Michael Kochell <kochelmj@rose-hulman.edu>
2014-03-27 15:35:01 -0400

    More MapTile tests added

--- src/main/MapTileTest.java
+++ src/main/MapTileTest.java
@@ -5,21 +5,31 @@ import org.junit.Test;
 
 
 /**
- * TODO Put here a description of what this class does.
+ * Tests the functionality of creating and rotating different map tile types.
  *
- * @author watersdr.
+ * @author watersdr, kochelmj.
  *         Created Mar 26, 2014.
  */
 public class MapTileTest {
 
 	@Test
-	public void testBlankTileCreation() {
+	public void testBlankTileCreation() 
+	{
 		TileCell[][] grid = MapTile.createBlankGrid();
-		assertFalse(grid[0][0] == null);
+		
+		for(int i = 0; i < grid.length; i++)
+		{
+			for (int j = 0; j < grid[i].length; j++)
+			{
+				assertFalse(grid[i][j].getAcessible());
+			}
+		}
+		
 	}
 	
 	@Test
-	public void testCreateQuad(){
+	public void testCreateQuad()
+	{
 		MapTile mapTile = new MapTile("quad", null);
 		String quadFormation = "false true false\ntrue true true\nfalse true false\n";
 		
@@ -27,7 +37,8 @@ public class MapTileTest {
 	}
 	
 	@Test
-	public void testCreateAndRotateL(){
+	public void testCreateAndRotateL()
+	{
 		MapTile mapTile = new MapTile("L", null);
 		String originalLFormation = "false false false\nfalse true true\nfalse true false\n";
 		String firstRotateLFormation = "false true false\nfalse true true\nfalse false false\n";
@@ -51,5 +62,55 @@ public class MapTileTest {
 		
 		
 	}
-
+	
+	@Test
+	public void testCreateandRotateTetris()
+	{
+		MapTile mapTile = new MapTile("tetris", null);
+		String originalTetrisFormation = "false false false\ntrue true true\nfalse true false\n";
+		String firstRotateTetrisFormation = "false true false\nfalse true true\nfalse true false\n";
+		String secondRotateTetrisFormation = "false true false\ntrue true true\nfalse false false\n";
+		String thirdRotateTetrisFormation = "false true false\ntrue true false\nfalse true false\n";
+		String fourthRotateTetrisFormation = originalTetrisFormation;
+		
+		assertEquals(mapTile.toString(), originalTetrisFormation);
+		
+		mapTile.rotateTile();
+		assertEquals(mapTile.toString(), firstRotateTetrisFormation);
+		
+		mapTile.rotateTile();
+		assertEquals(mapTile.toString(), secondRotateTetrisFormation);
+		
+		mapTile.rotateTile();
+		assertEquals(mapTile.toString(), thirdRotateTetrisFormation);
+		
+		mapTile.rotateTile();
+		assertEquals(mapTile.toString(), fourthRotateTetrisFormation);
+	}
+	
+	@Test
+	public void testCreateAndRotateStraight()
+	{
+		MapTile mapTile = new MapTile("straight", null);
+		String originalStraightFormation = "false false false\ntrue true true\nfalse false false\n";
+		String firstRotateStraightFormation = "false true false\nfalse true false\nfalse true false\n";
+		String secondRotateStraightFormation = originalStraightFormation;
+		String thirdRotateStraightFormation = firstRotateStraightFormation;
+		String fourthRotateStraightFormation = originalStraightFormation;
+		
+		assertEquals(mapTile.toString(), originalStraightFormation);
+		
+		mapTile.rotateTile();
+		assertEquals(mapTile.toString(), firstRotateStraightFormation);
+		
+		mapTile.rotateTile();
+		assertEquals(mapTile.toString(), secondRotateStraightFormation);
+		
+		mapTile.rotateTile();
+		assertEquals(mapTile.toString(), thirdRotateStraightFormation);
+		
+		mapTile.rotateTile();
+		assertEquals(mapTile.toString(), fourthRotateStraightFormation);
+	}
+	
 }
