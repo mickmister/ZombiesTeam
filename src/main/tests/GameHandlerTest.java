4bea2d92b509a4fe330fe198af0237b245fbdb72
Michael Kochell <kochelmj@rose-hulman.edu>
2014-04-02 14:56:10 -0400

    GameHandlerTest modified

--- src/main/tests/GameHandlerTest.java
+++ src/main/tests/GameHandlerTest.java
@@ -2,7 +2,9 @@ package main.tests;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import gui.ImageManager;
 import main.GameHandler;
+import main.GameHandler.GameState;
 
 import org.junit.Test;
 
@@ -13,6 +15,7 @@ public class GameHandlerTest
 	{
 		for (int i = 0; i <= 4; i += 1)
 		{
+			new ImageManager();
 			GameHandler test = new GameHandler(i);
 			assertNotNull(test);
 		}
@@ -21,11 +24,28 @@ public class GameHandlerTest
 	@Test
 	public void testNextTurn()
 	{
+		new ImageManager();
 		GameHandler test = new GameHandler(2);
 		assertEquals(0, test.getTurn());
 		test.nextTurn();
 		assertEquals(1, test.getTurn());
 		test.nextTurn();
 		assertEquals(0, test.getTurn());
+	}
+	
+	@Test
+	public void testNextState()
+	{
+		new ImageManager();
+		GameHandler test = new GameHandler(2);
+		assertEquals(GameState.tilePlacement, test.getCurrentState());
+		test.nextGameState();
+		assertEquals(GameState.zombiePlacement, test.getCurrentState());
+		test.nextGameState();
+		assertEquals(GameState.playerMovement, test.getCurrentState());
+		test.nextGameState();
+		assertEquals(GameState.zombieMovement, test.getCurrentState());
+		test.nextGameState();
+		assertEquals(GameState.tilePlacement, test.getCurrentState());
 	}
 }
