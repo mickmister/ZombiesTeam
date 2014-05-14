08db9284d50fd666a41e3df0d4230637a7e91a21
Jacob Ryan <ryanjm@rose-hulman.edu>
2014-05-14 15:43:23 -0400

    Added name of special buildings to be drawn in top left corner of MapTile.

--- src/view/MapTileView.java
+++ src/view/MapTileView.java
@@ -3,14 +3,21 @@ package view;
 import java.awt.*;
 
 import main.*;
+import main.MapTile.Shape;
 
 public class MapTileView
 {
 	private MapTile mapTile;
+	private String specialName;
 	
 	public MapTileView(MapTile tile)
 	{
 		this.mapTile = tile;
+		this.specialName = "";
+		if (Shape.special.equals(this.mapTile.getShape()))
+		{
+			this.specialName = hackFromEnum();
+		}
 	}
 	
 	public void draw(Graphics2D graphics, int xPos, int yPos, boolean isTemp)
@@ -30,7 +37,27 @@ public class MapTileView
 				view.draw(graphics, cellX, cellY, isTemp, tempZombie, tempBullet, tempLife, tempMove);
 			}
 		}
+		graphics.setColor(Color.BLACK);
+		graphics.drawString(this.specialName, xPos * 240 + 11, yPos * 240 + 21);
 		graphics.setColor(Color.WHITE);
+		graphics.drawString(this.specialName, xPos * 240 + 10, yPos * 240 + 20);
+		
 		graphics.drawRect(xPos * 240, yPos * 240, 240, 240);
+	}
+	
+	private String hackFromEnum()
+	{
+		char[] name = this.mapTile.getSpecialName().name().toCharArray();
+		String output = "";
+		output += Character.toUpperCase(name[0]);
+		for (int i = 1; i < name.length; i += 1)
+		{
+			if (Character.isUpperCase(name[i]))
+			{
+				output += " ";
+			}
+			output += name[i];
+		}
+		return output;
 	}
 }
