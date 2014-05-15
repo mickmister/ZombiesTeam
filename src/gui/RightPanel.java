b99c9c822ca03ac33bec95e063547221da5898d9
Jacob Ryan <ryanjm@rose-hulman.edu>
2014-05-14 23:54:47 -0400

    Made right panel more compact while displaying cards in hand, active cards, and discarded cards.

--- src/gui/RightPanel.java
+++ src/gui/RightPanel.java
@@ -11,39 +11,65 @@ public class RightPanel extends JPanel
 	{
 		setLayout(new GridLayout(0, 3, 10, 0));
 		setBorder(new EmptyBorder(10, 10, 0, 10));
-		setPreferredSize(new Dimension(800, 1));
+		setPreferredSize(new Dimension(700, 1));
 		
 		JPanel inHandPanel = new JPanel();
-		inHandPanel.setLayout(new BorderLayout());
+		inHandPanel.setLayout(new GridLayout(1, 1));
 		inHandPanel.setBorder(new TitledBorder("Cards in your hand"));
-		
-		JPanel inHandButtons = new JPanel();
-		inHandButtons.setLayout(new GridLayout(3, 1, 0, 10));
-		for (int i = 0; i < 3; i += 1)
-		{
-			inHandButtons.add(new EventCardButton(i));
-		}
-		
-		JPanel inHandSwaps = new JPanel();
-		inHandSwaps.setLayout(new GridLayout(3, 1, 0, 10));
-		for (int i = 0; i < 3; i += 1)
-		{
-			inHandSwaps.add(new SwapCardButton(i));
-		}
-		
-		inHandPanel.add(inHandButtons, BorderLayout.CENTER);
-		inHandPanel.add(inHandSwaps, BorderLayout.LINE_END);
+		inHandPanel.add(createInHandPanel());
 		
 		JPanel activePanel = new JPanel();
 		activePanel.setLayout(new BorderLayout());
 		activePanel.setBorder(new TitledBorder("Active cards"));
+		activePanel.add(createActivePanel());
 		
 		JPanel discardedPanel = new JPanel();
 		discardedPanel.setLayout(new BorderLayout());
 		discardedPanel.setBorder(new TitledBorder("Discarded cards in effect"));
+		discardedPanel.add(createDiscardedPanel());
 		
 		add(inHandPanel);
 		add(activePanel);
 		add(discardedPanel);
+	}
+	
+	private JPanel createInHandPanel()
+	{
+		JPanel panel = new JPanel();
+		panel.setBorder(new EmptyBorder(10, 10, 10, 10));
+		panel.setLayout(new GridLayout(3, 1, 0, 10));
+		for (int i = 0; i < 3; i += 1)
+		{
+			JPanel subPanel = new JPanel();
+			subPanel.setLayout(new BorderLayout());
+			subPanel.add(new EventCardButton(i), BorderLayout.CENTER);
+			subPanel.add(new SwapCardButton(i), BorderLayout.PAGE_END);
+			panel.add(subPanel);
+		}
+		return panel;
+	}
+	
+	private JPanel createActivePanel()
+	{
+		JPanel panel = new JPanel();
+		panel.setBorder(new EmptyBorder(10, 10, 10, 10));
+		panel.setLayout(new GridLayout(3, 1, 0, 10));
+		for (int i = 0; i < 3; i += 1)
+		{
+			panel.add(new ActiveCardButton(i));
+		}
+		return panel;
+	}
+	
+	private JPanel createDiscardedPanel()
+	{
+		JPanel panel = new JPanel();
+		panel.setBorder(new EmptyBorder(10, 10, 10, 10));
+		panel.setLayout(new GridLayout(3, 1, 0, 10));
+		for (int i = 0; i < 3; i += 1)
+		{
+			panel.add(new ActiveCardButton(i));
+		}
+		return panel;
 	}
 }
